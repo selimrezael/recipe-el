a95ab1a2678ebc07a0aabb1db01afd5e42866bc3
Mirza Selim Reza <selimrezael@gmail.com>
2016-11-01 20:28:32 +0600

    recipe update and destroy done

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -22,6 +22,22 @@ class RecipesController < ApplicationController
   	end
   end
 
+  def edit
+  end
+  
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @recipe.destroy
+    redirect_to root_path, notice: "Successfully deleted the recipe"
+  end
+
   private
 
   def find_recipe
